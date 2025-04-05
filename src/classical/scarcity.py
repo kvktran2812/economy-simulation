cba9702f97e51a74ce95a50e1e80c0585d255610
kvktran2812 <kvktran@gmail.com>
2025-04-04 23:49:40 -0400

    add production curve

---
+++ src/classical/scarcity.py
@@ -0,0 +1,26 @@
+import numpy as np
+from typing import List
+
+# TODO: Increase dimension
+def prodduction_possibilities_curve(production: List[int], mode='linear'):
+    n = len(production)
+
+    if n == 0:
+        raise ValueError("No production at all, empty list can not be productions")
+    else:
+        validate_production(production)
+    
+    if n == 1:
+        return production
+    
+    if n > 2:
+        raise NotImplementedError("Solution for dimension above 2 is not implemented")
+    
+    return -production[0]/production[1]
+
+def validate_production(production: List[int]):
+    n = len(production)
+
+    for i in range(n):
+        if production[i] <= 0:
+            raise ValueError(f"Production can not be less than or equal to zero. Production of index {i} is {production[i]}")
